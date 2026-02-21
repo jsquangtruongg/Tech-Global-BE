77c3c6cb303dd9a50399d96dd05d63d48b06c0f7
jstruongg <nguyenqtthangbinh@gmail.com>
2026-02-21 21:06:46 +0700

    done seender

--- src/migrations/016-create-common-error.ts
+++ src/migrations/016-create-common-error.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
+export default = {
   up: async (queryInterface: any, Sequelize: any) => {
     await queryInterface.createTable("common_errors", {
       id: {
